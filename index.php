0ec2e4b7de0307710021b2fd3b9b2ecf7c37c214
Cristovao <ml.manutencaolira@gmail.com>
2020-11-02 15:09:39 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Manutenção Lira é uma empresa que propociona suporte técnico em informática para clientes domésticos e pequenas empresas e acessórios que podemos oferecer dentro de nossa área, procurando na medida do impossível, adaptar serviços existentes para as suas necessidades, promvendo sempre a sua satisfação. A Manutenção Lira também trabalha com criação de sites dentro das normas da W3C, com recursos de linguagem de alta tecnologia.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MANUTENÇÃO LIRA | Suporte Técnico em Informática</title>
+        <link rel="stylesheet" href="css/materialize.css"/>
+        <link rel="stylesheet" href="css/boot.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body>
+        
+        <header class="main_header container">
+            <div class="content">
+                <h1 class="main_logo fontzero fl-left">
+                    <a title="MANUTENÇÃO LIRA | Suporte Técnico em Informática" href="#!">
+                        MANUTENÇÃO LIRA | Suporte Técnico em Informática
+                    </a>
+                </h1>
+
+                <ul class="main_menu fl-right">
+                    <li><a title="Manutenção Lira | Suporte Técnico em Informática" href="#!">HOME</a></li>
+                    <li><a title="Sobre a Empresa Manutenção Lira" href="#!">EMPRESA</a></li>
+                    <li><a title="Sobre os Serviços da Manutenção Lira" href="#!">SERVIÇOS</a></li>
+                    <li><a title="Fique por dentro das Notícias" href="#!">NOTÍCIAS</a></li>
+                    <li><a title="Fale Conosco" href="#!">BOOK ONLINE</a></li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+        </header>
+        
+        <section class="container">
+            <h1 class="fontzero">Últimas Notícias</h1>
+            <div class="slider container">
+                
+                <ul class="slides">
+                    <li>
+                        <img src="img/slider1.jpg">
+                        <header class="caption center-align">
+                            <img src="slider_item img/logo.png"/>
+                            <h2>SEJA BEM VINDO(a)!</h1>
+                            <p class="tagline light grey-text text-lighten-3">O Seu Suporte Técnico em Informática.</p>
+                        </header>
+                    </li>
+                    
+                    <li>
+                        <img src="img/slider1.jpg">
+                        <header class="caption center-align">                            
+                            <h2>FAÇA UMA MANUTENÇÃO PREVENTIVA!</h2>
+                            <p class="tagline light grey-text text-lighten-3">Realizamos um Chercap em seu Equipamento, previnindo e mantendo a vida util do mesmo.</h5>
+                        </header>
+                    </li>
+                    
+                    <li>
+                        <img src="img/slider1.jpg">
+                        <header class="caption center-align">
+                            <h2>TRABALHAMOS COM CRIAÇÕES DE SITES!</h2>
+                            <p class="tagline light grey-text text-lighten-3">A Manutenção Lira Também trabalha com criações de sites com as melhores tecnologias e dentro do padrão da W3C.</h5>
+                        </header>
+                    </li>      
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+        </section>
+        
+        <section class="container">
+            <div class="content">
+                <h1 class="fontzero">Empresa</h1>
+                <header class="main_tec_title sectiontitle">
+                        <h2>Quem Somos!</h2>
+                        <p class="tagline">Empresa</p>
+                </header>
+                
+               
+                <p class="center-align fontsizel">Manutenção Lira é uma empresa de Suporte Técnico em Informática para clientes domésticos e pequenas empresas.</p>
+                
+                <br>
+                
+                <article class="main_tec_item box box-medium hoverable">
+                    <h1>Missão</h1>
+                    <p>Atuar com padrões de excelência no serviços prestados aos clientes, aperfeiçoando processos, habilidades humanas e profissionais.</p>
+                </article>
+                
+                <article class="main_tec_item box box-medium hoverable">
+                    <h1>Visão</h1>
+                    <p>Ser reconhecido como uma das melhores fornecedoras de serviços na área de suporte técnico em informática, superando sempre as expectativas de nossos clientes.</p>
+                </article>
+                
+                <article class="main_tec_item box box-medium last hoverable">
+                    <h1>Valores</h1>
+                    <p>Compromisso com Nossos Clientes.<br>
+                    Ética Profissional.<br>
+                    Profissionalismo em Nossos Serviços.</p>
+                </article>
+                
+                <div class="center-align">
+                    <a href="#!" class="btn waves-effect">conheça</a>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </section>
+        
+        <section class="container bg-yellow">
+            <div class="content">
+                <h1 class="fontzero">Nossos Serviços</h1>
+                <header class="main_serv_title sectiontitle">
+                        <h2>O que realizamos!</h2>
+                        <p class="tagline">Nossos Serviços</p>
+                </header>
+                                
+                <article class="main_serv_item box box-small">
+                    <i class="material-icons">computer</i>
+                    <h1>Notebooks</h1>
+                </article>
+
+                <article class="main_serv_item box box-small">
+                    <i class="material-icons">desktop_windows</i>
+                    <h1>Computadores</h1>
+                </article>
+
+                <article class="main_serv_item box box-small">
+                    <i class="material-icons">build</i>
+                    <h1>Manutenção</h1>
+                </article>
+
+                <article class="main_serv_item box box-small last">
+                    <i class="material-icons">verified_user</i>
+                    <h1>Limpeza Completa</h1>
+                </article>
+
+                <article class="main_serv_item box box-small">
+                    <i class="material-icons">assignment_turned_in</i>
+                    <h1>Diagnóstico</h1>
+                </article>
+
+                <article class="main_serv_item box box-small">
+                    <i class="material-icons">important_devices</i>
+                    <h1>Criação de Sites</h1>
+                </article>
+                
+                <article class="main_serv_item box box-small">
+                    <i class="material-icons">store</i>
+                    <h1>Atendimento a Domicílio</h1>
+                </article>
+                
+                <article class="main_serv_item box box-small last">
+                    <i class="material-icons">perm_identity</i>
+                    <h1>Suporte Online</h1>
+                </article>
+                
+                <div class="center-align">
+                    <a href="#!" class="main_serv_btn btn waves-effect">Saiba Mais</a>
+                </div>              
+                <div class="clearfix"></div>
+            </div>
+        </section>
+        
+        <section class="container">
+            <h1 class="fontzero">Notícias</h1>
+            <div class="content">
+                <header class="main_not_title sectiontitle">
+                        <h2>Fique por Dentro</h2>
+                        <p class="tagline">Notícias</p>
+                </header>
+                
+                <div class="box box-forthree">
+                    <article>
+                        <img title="" alt="" src=""/>
+                        <h1>Criação de Site</h1>
+                        <p></p>
+                    </article>
+
+                    <article>
+                        <img title="" alt="" src=""/>
+                        <h1>Criação de Site</h1>
+                        <p></p>
+                    </article>
+                </div>
+                
+                <aside class="bg-black box box-forthreesmall center-align">
+                    <h1>Últimas Notícias</h1>
+                    
+                    <article>
+                        <time></time>
+                        <h1>Indicações de Filmes</h1>
+                        <p></p>
+                    </article>
+                    
+                    <article>
+                        <time></time>
+                        <h1>Novas Tecnologias</h1>
+                        <p></p>
+                    </article>
+                    
+                    <article>
+                        <time></time>
+                        <h1>Lançamentos</h1>
+                        <p></p>
+                    </article>
+                    
+                    <a href="#!" class="btn waves-effect">Saiba Mais</a>
+                </aside>
+                
+                <div class="clearfix"></div>
+            </div>
+        </section>
+        
+        <section class="container content-page bg-black">
+            <div class="content">
+                <header class="main_pro_title center-align">
+                    <h1>Newsletter</h1>
+                    <p class="tagline">Receba Nossas Promoções</p>
+                </header>
+                <form class="main_newsletter">
+                    <input class="radius" type="text" name="nome" title="Informe Seu Nome" placeholder="Informe Seu Nome" required/>
+                    <input class="radius" type="email" name="email" title="Informe Seu Email" placeholder="Informe Seu Email" required/>
+                    
+                    <button href="#!" class="radius">Enviar</button>
+                </form>
+                <div class="clearfix"></div>
+            </div>
+        </section>
+        
+        <footer class="container bg-red">            
+            <section class="content">
+                <h1 class="fontzero">Rodapé do Site</h1>
+                <article class="main_footer box box-medium">
+                    <h1>Nosso Endereço:</h1>
+                    <p><b>Horário de Atendimento:</b> 08:00 às 18:00 hrs</p>
+                    <p><b>Rua:</b> Dr. Silvino Olavo, nº 38</p>
+                    <p>58135-000 | Esperança-PB</p>
+                    <p>Brasil</p>
+                </article>
+                
+                <article class="main_footer box box-medium">
+                    <h1>Nosso Contato:</h1>
+                    <p><b>Email:</b> ML_servicos@outlook.com</p>
+                    <p><b>Whatsapp:</b> (83) 9 9837 - 9516</p>
+                    <p><b>fone:</b> (83) 9 9837 - 9516</p>
+                </article>
+                
+                <article class="main_footer box box-medium last">
+                    <h1>Manutenção Lira nas Redes:</h1>
+                    
+                    <div class="link_facebook box box-full">
+                        <a title="Compartilhe no Facebook!" href="#!" style="color: #fff; font-size: 1em;">Compartilhe no Facebook!</a>
+                    </div>
+                    
+                    <div class="link_goole box box-full">
+                        <a title="Recomende no Google!" href="#!" style="color: #fff; font-size: 1em;">Recomende no Google!</a>
+                    </div>
+                    
+                    <div class="link_twitter box box-full">
+                        <a title="Conte isso no Twitter!" href="#!" style="color: #fff; font-size: 1em;">Conte isso no Twitter!</a>
+                    </div>
+                </article>
+                
+                <div class="clearfix"></div>
+            </section>
+             <div class="footer-copyright">
+                <p>Copyright &copy 2017 - <?= date('Y'); ?> Criado por Cristovão Lira Braga | Manutenção Lira, Todos Os Direitos Reservados!</p>
+             </div>
+        </footer>
+        
+        <script src="js/jquery.min.js"></script>
+        <script src="js/materialize.js"></script>
+        <script src="js/script.js"></script>
+    </body>
+</html>
